21f03ca9c7629cfd678fc1e50bff162dc37f3fde
SwitchDoc Labs <jshovic@switchdoc.com>
2020-01-12 10:15:15 -0800

    V002 - added config.py into archive

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 Python Drivers for SolarMAX
 
 SwitchDoc Labs <BR>
-August 2019<BR>
+January 2020<BR>
+V003 - Added config.py into archive
+
 V002 - First Release - fixed current directions for SolarMAX
 
 sudo python testWXLink.py will read the LoRa and decode the data coming from the SolarMAX system.
---
+++ config.py
@@ -0,0 +1,17 @@
+
+#
+#
+# configuration file - contains customization for exact system
+#
+
+# it is a good idea to copy this file into a file called "conflocal.py" and edit that instead of this one.  This file is wiped out if you update SkyWeather.
+
+SWDEBUG = True
+
+SWVERSION = "0.0"
+import uuid 
+  
+# WXLink and SolarMAX configuration
+SolarMAX_Present = True
+Dual_MAX_WXLink = False
+
